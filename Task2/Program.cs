33d3ec26f0a8518754cb36510e489aaa94a7f163
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-14 17:20:56 +0200

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()),
+                b = int.Parse(Console.ReadLine()),
+                c = int.Parse(Console.ReadLine()),
+                maxValue;
+            if (a > b && a > c)
+                maxValue = a;
+            else
+                    if (b > c)
+                maxValue = b;
+            else
+                maxValue = c;
+            
+            Console.WriteLine("Max Value = "+maxValue);
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameArray = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", };
+            int inputData,sizeArrey=0, temp;
+                                    
+            Console.WriteLine("Enter number:");   
+             inputData = int.Parse(Console.ReadLine());
+            temp = inputData;
+            while (temp != 0)
+            {   
+                temp /=10;
+                sizeArrey++;
+            }
+            
+            int[] numberArrey = new int[sizeArrey];
+            int i=0;
+            temp = inputData;
+            
+            while (temp != 0)
+            { 
+                i++;
+                numberArrey[sizeArrey - i] = temp % 10;
+                temp /= 10;                                           
+            }
+            
+             for (int n = 0; n < sizeArrey; n++)
+             {
+                Console.WriteLine(nameArray[numberArrey[n]]);                
+             }
+
+        }
+    }
+}
