35b1a247168a4c385c0522fdc24458d70ed34f5e
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-15 20:57:21 +0200

    create task11

---
+++ Task11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static ulong Factorial(ulong x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number.");
+            ulong enterNumber = ulong.Parse(Console.ReadLine());
+            ulong factorial = 0;
+            factorial = Factorial(enterNumber);
+            Console.WriteLine(enterNumber+"! = "+ factorial);
+        }
+    }
+}
