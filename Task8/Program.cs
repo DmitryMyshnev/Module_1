33501e23d2dc446b22436b665c05cf8e3dcc5c71
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-15 14:14:57 +0200

    the end task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -6,7 +6,35 @@ namespace Task8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int[] array1 = new int[10];
+            int[] array2 = new int[10];
+            
+            for (int i = 0; i < 10; i++)
+            {
+                array1[i] = rnd.Next(0,10);
+                array2[i] = rnd.Next(0,10);
+            }
+           foreach (var item in array1)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("");
+            foreach (var item in array2)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 10; i++)
+            {
+                if (array1[i] == array2[i])
+                {                 
+                    Console.Write("^ ");
+                }
+                else
+                    Console.Write("  ");
+            }
+           
         }
     }
 }
