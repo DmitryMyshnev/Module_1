6d6c2567cb31dceff858aebeebb725fd1b0e80b8
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-17 12:37:16 +0200

    the end task17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -6,7 +6,22 @@ namespace Task17
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text, simbol;
+            int index;
+            Console.WriteLine("Enter your text.");
+            text = Console.ReadLine();
+            Console.WriteLine("Enter substring for remove.");
+            simbol = Console.ReadLine();
+            index = text.IndexOf(simbol);
+
+            if (index < 0)
+                Console.WriteLine("Simbol is not found");
+            else
+            {
+                Console.WriteLine("New Text:");
+                text = text.Remove(index, simbol.Length);
+                Console.WriteLine(text);
+            }
         }
     }
 }
