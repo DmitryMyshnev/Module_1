f86d993e5bcad6019f26dc8a7c632e392d1ad8cd
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-17 09:30:25 +0200

    the end task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -25,42 +25,57 @@ namespace Task12
         }
         static void Insert(int[] arr, int size)
         {
-            int buff = 0;
-            int tmp=0;
-            for (int n = 0; n < size - 1; n++)           
-                for (int i = 0; i < size - 1; i++)               
+            int buff ;
+            int tmp;
+           // for (int n = 0; n < size - 1; n++)           
+                for (int i = 0; i < size - 1 ; i++)               
                     if (arr[i] > arr[i + 1])
-                    { 
-                        tmp = i;
-                        while(arr[tmp]< arr[i+1] && tmp !=0)
-                            tmp--;
+                    {             
                         buff = arr[i+1];
                         arr[i+1] = arr[i];
-                        arr[tmp] = buff;
-                    } 
+                        arr[i] = buff;
+                        tmp = i;
+                        if (tmp != 0)
+                            while (arr[tmp] < arr[tmp - 1])
+                        {
+                            buff = arr[tmp];
+                            arr[tmp] = arr[tmp-1];
+                            arr[tmp-1] = buff;
+                            if(tmp!=1)
+                             tmp--;
+                        }
+                        
+                    }
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
         }
         static void Main(string[] args)
         {
-            int sizeOfArrey = 5;
+            int sizeOfArrey = 10;
             string choice;
             
-            int[] array = new int[sizeOfArrey]{5,4,3,2,1,0};
+            int[] array = new int[sizeOfArrey];
             
-           // Random rnd = new Random();
+            Random rnd = new Random();
             Console.WriteLine("Array:");
             for (int i = 0; i < sizeOfArrey; i++)
             {
-               // array[i] = rnd.Next(0, 100);
+                array[i] = rnd.Next(0, 100);
                 Console.Write(array[i]+" ");
             }
 
             Console.WriteLine("") ;
-           // Console.WriteLine("Made of choice for sort: 'bothle' or 'insert'");
+            Console.WriteLine("Made of choice for sort: 'bothle' or 'insert'");
             
-           // choice = Console.ReadLine();
-           // if (choice == "bothle")
-               // Bothle(array,sizeOfArrey);
+            choice = Console.ReadLine();
+            if (choice == "bothle")
+               Bothle(array,sizeOfArrey);
+            else
+                if(choice == "insert")
                Insert(array,sizeOfArrey);
+             
                      
         }
     }
