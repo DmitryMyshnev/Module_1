a151f2df2d65eab1b09aa7b2cc3ef16e8c8d8f8a
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-17 16:04:36 +0200

    rhe end task20

---
+++ Task20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string simbol;
+            char[] anyOf;
+            string[] newText;
+
+            Console.WriteLine("Enter your text.");
+            text = Console.ReadLine();
+            Console.WriteLine("Enter your separate simbol.");
+            simbol = Console.ReadLine();
+            anyOf = simbol.ToCharArray(0,1);
+            newText = text.Split(anyOf);
+            foreach (var item in newText)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
