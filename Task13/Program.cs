bc70dd67b61a4ded625e0351caa4b3b9e5c742e8
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-17 10:21:03 +0200

    the end task13

---
+++ Task13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task13
+{
+    class Program
+    {
+        static int BinarySearch(int[] arr, int val, int size)
+        {
+            int search;
+            Array.Sort(arr);
+            foreach (var item in arr)
+                Console.Write(item+" ");
+            Console.WriteLine("");
+            if (arr[0] == val)
+                return 0;
+            else
+            if (arr[size - 1] == val)
+                return size - 1;
+            search = Array.BinarySearch(arr, val);
+            if (search < 0)
+                return -1; 
+            else
+                return search;
+
+        }
+        static void Main(string[] args)
+        {
+            int sizeOfArrey = 10;           
+            int[] array = new int[sizeOfArrey];
+            int valueOfSearch;
+            int result;
+            Random rnd = new Random();
+            Console.WriteLine("Array:");
+            for (int i = 0; i < sizeOfArrey; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Enter number for search.");
+            valueOfSearch = int.Parse(Console.ReadLine());
+            result = BinarySearch(array, valueOfSearch,sizeOfArrey);
+            if (result == -1)           
+                Console.WriteLine("Value is not found");
+            else
+                Console.WriteLine("The item you are looking for is: " + result);
+            
+        }
+    }
+}
