c7eaf945d6aef4b348bba2507497d8878bafd710
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-17 13:00:36 +0200

    the end task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -6,7 +6,19 @@ namespace Task18
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text; 
+            char[]  simbol;
+            int sizeOfText;
+            Console.WriteLine("Enter your text.");
+            text = Console.ReadLine();
+            sizeOfText = text.Length;
+            simbol = text.ToCharArray(0,sizeOfText);
+            Console.WriteLine("New Text:");
+            for (int i = sizeOfText-1; i >= 0; i--)
+            {
+               Console.Write(simbol[i]);
+            }
+            
         }
     }
 }
