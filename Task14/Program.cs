f5173307dc7187142cc97ea237e457692e4010ec
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-17 10:55:26 +0200

    the end task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -6,7 +6,24 @@ namespace Task14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text,simbol;
+           
+            Console.WriteLine("Enter your text.");
+            text = Console.ReadLine();
+            Console.WriteLine("Enter simbol for search.");
+            simbol = Console.ReadLine();
+            int index = text.IndexOf(simbol);
+            if (index < 0)
+                Console.WriteLine("Simbol is not found");
+            else
+            {
+                Console.WriteLine(text);
+                for (int i = 0; i < index; i++)
+                    Console.Write(" ");
+
+                Console.WriteLine("^");
+                Console.WriteLine("Index of your simbol is: " + index);
+            }
         }
     }
 }
