cb858ecfecf246ec7a684848c7d668f25b23e9c4
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2019-12-17 12:27:33 +0200

    the end task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -6,7 +6,29 @@ namespace Task16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text, simbol;
+            int index;
+            Console.WriteLine("Enter your text.");
+            text = Console.ReadLine();
+            Console.WriteLine("Enter substring for search.");
+            simbol = Console.ReadLine();
+            index = text.IndexOf(simbol);
+            if (index < 0)
+                Console.WriteLine("Simbol is not found");
+            else
+            {
+                Console.WriteLine(text);
+                for (int i = 0; i < index; i++)
+                    Console.Write(" ");
+                for (int i = 0; i < simbol.Length; i++)
+                {
+                   Console.Write("^");
+                }
+                Console.WriteLine("");
+                Console.Write("Indexs of your substring is: ");
+                for (int i = index; i < (index + simbol.Length); i++)
+                    Console.Write(i+" ");
+            }
         }
     }
 }
